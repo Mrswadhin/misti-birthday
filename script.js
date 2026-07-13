00a525086ca9149897faedbeb801c0a0abe9023b
Mrswadhin <swadhindaskrishnagar@gmail.com>
2026-07-13 19:26:34 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,75 @@
+function start() {
+
+    document.querySelector(".overlay").innerHTML = `
+
+    <h1>💛 Happy Birthday Misti 💛</h1>
+
+    <p style="font-size:22px;
+    color:#444;
+    margin-top:20px;
+    line-height:1.8;">
+
+    Every beautiful moment becomes even more beautiful
+    because you are a part of it.
+
+    <br><br>
+
+    Today is your day...
+
+    <br>
+
+    Enjoy every smile,
+    every blessing,
+    every moment.
+
+    <br><br>
+
+    ❤️ A Special Surprise is Coming... ❤️
+
+    </p>
+
+    <br>
+
+    <button onclick="nextPage()">Continue ➜</button>
+
+    `;
+
+}
+
+function nextPage(){
+
+    document.querySelector(".overlay").innerHTML=`
+
+    <h1>Coming Soon...✨</h1>
+
+    <p style="font-size:22px;
+    line-height:1.8;
+    color:#444;">
+
+    Photo Gallery 📸
+
+    <br>
+
+    Beautiful Letter 💌
+
+    <br>
+
+    Birthday Cake 🎂
+
+    <br>
+
+    Gift Box 🎁
+
+    <br>
+
+    Surprise Video 🎥
+
+    <br><br>
+
+    <b>— Swadhin da 💙</b>
+
+    </p>
+
+    `;
+
+}
